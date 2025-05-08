e38e4689b62022123bff7479b5f24cc5514f7d02
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-05-08 03:22:59 -0700

    add register command

--- src/commands/users.ts
+++ src/commands/users.ts
@@ -1,6 +1,7 @@
 import { setUser } from "../config";
+import { createUser } from "../lib/db/queries/users";
 
-export function handlerLogin(cmdName: string, ...args: string[]) {
+export async function handlerLogin(cmdName: string, ...args: string[]) {
   if (args.length !== 1) {
     throw new Error(`usage: ${cmdName} <name>`);
   }
@@ -8,4 +9,19 @@ export function handlerLogin(cmdName: string, ...args: string[]) {
   const userName = args[0];
   setUser(userName);
   console.log("User switched successfully!");
+}
+
+export async function handlerRegister(cmdName: string, ...args: string[]) {
+  if (args.length != 1) {
+    throw new Error(`usage: ${cmdName} <name>`);
+  }
+
+  const userName = args[0];
+  const user = await createUser(userName);
+  if (!user) {
+    throw new Error(`User ${userName} not found`);
+  }
+
+  setUser(user.name);
+  console.log("User created successfully!");
 }
