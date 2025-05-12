916d7408f6ad38dc7dc21c52e4c283db5bf6f000
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-05-12 14:14:21 -0700

    add browse command

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ import {
   handlerUnfollow,
 } from "./commands/feed_follows";
 import { middlewareLoggedIn } from "./middleware";
+import { handlerBrowse } from "./commands/browse";
 
 async function main() {
   const args = process.argv.slice(2);
@@ -55,6 +56,11 @@ async function main() {
     commandsRegistry,
     "unfollow",
     middlewareLoggedIn(handlerUnfollow),
+  );
+  registerCommand(
+    commandsRegistry,
+    "browse",
+    middlewareLoggedIn(handlerBrowse),
   );
 
   try {
